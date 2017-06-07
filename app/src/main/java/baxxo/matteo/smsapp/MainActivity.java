fc77bf422a7cdad9b0ae2ee1ffb021074d8083f3
Baxxo <matteobasso9@gmail.com>
2017-06-07 23:11:26 +0200

    float button fade in

--- app/src/main/java/baxxo/matteo/smsapp/MainActivity.java
+++ app/src/main/java/baxxo/matteo/smsapp/MainActivity.java
@@ -10,7 +10,6 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Rect;
 import android.media.AudioManager;
 import android.os.Build;
 import android.os.Handler;
@@ -29,17 +28,14 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.TranslateAnimation;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -51,8 +47,6 @@ import android.widget.TimePicker;
 import java.util.ArrayList;
 import java.util.Calendar;
 
-import static baxxo.matteo.smsapp.R.id.action0;
-import static baxxo.matteo.smsapp.R.id.appbar;
 import static baxxo.matteo.smsapp.R.id.container;
 
 
